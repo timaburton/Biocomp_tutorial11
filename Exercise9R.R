92d8ce6e275b2890744ed288e31afbb7722da784
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-15 21:45:39 -0500

    Creates a data frame as an output for easier reading

--- Exercise9R.R
+++ Exercise9R.R
@@ -3,7 +3,7 @@
 
 #VarCof is a function that reads data from each file in the specified directory 
 #and calculates the coefficient of variation. By default, 50 observations are required
-#to get an output.
+#to get an output. The column of interest must be numerical.
 
 #Usage: VarCof(dir = "full directory map", column = "number of column of interest", 
 #x = minimum number of observations, req = is override on or off (off by default))
@@ -12,22 +12,22 @@ VarCof<-function(dir, column, x=50, req="Yes"){
   #Set the working directory to the directory entered 
   setwd(dir)
   #List files in a variable
-  file.list<-list.files(dir)
+  File<-list.files(dir)
   #Empty vector to add results to
-  vector<-c()
+  Variation<-c()
   #For each of the files, pull out the column of interest
-  for (files in file.list){
+  for (files in File){
     #Read the file as a table so the column can be pulled out
-    file<-read.table(files, header = TRUE, sep=",", stringsAsFactors = FALSE)
+    file_i<-read.table(files, header = TRUE, sep=",", stringsAsFactors = FALSE)
     #Pull out the column of interest
-    interestcol<-file[, column]
+    interestcol<-file_i[, column]
     #Find the number of observations
     if(length(interestcol)>=x){ #More than or equal to X, no error message
       #Calculate, mean, standard deviation, and variation
       M<-mean(interestcol)
       SD<-sd(interestcol, na.rm = FALSE)
       Var<-SD/M
-      vector<-c(vector, Var)
+      Variation<-c(Variation, Var)
       }else if (length(interestcol)<x && req == "Yes"){ #Less than X, no override, gives warning 
       print("Error: There are not enough observations to calculate a reliable coefficient of variation")
         #vector<-c(vector, NA)
@@ -36,12 +36,12 @@ VarCof<-function(dir, column, x=50, req="Yes"){
         M<-Mean(interestcol)
         SD<-sd(interestcol)
         Var<-SD/M
-        vector<-c(vector, Var)
+        Variation<-c(Variation, Var)
       }else{
         print("Check arguement inputs")
       }
      }
-    return(vector)
+    return(data.frame(File, Variation))
   }
 
 
