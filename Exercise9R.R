ff684b716ba23057d96a89bb58db0834b43c9901
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-15 21:09:16 -0500

    Fixes minor bugs. Need column number to be the input

--- Exercise9R.R
+++ Exercise9R.R
@@ -1,52 +1,44 @@
 #Marlee Shaffer
 ##Exercise 9 
 
-#Write a function that takes a directory name as an argument called dir plus any other arguments required to
-#accomplish the specified task.
+#VarCof is a function that reads data from each file in the specified directory 
+#and calculates the coefficient of variation. By default, 50 observations are required
+#to get an output.
 
-#The function should read data from each file in the specified directory and calculate the coefficient of variation
-#(standard deviation divided by the mean) for a user specified column. These values should be returned as a
-#vector.
-
-#To calculate a reliable coefficient of variation we would like to have 50 observations, but we also don’t want
-#to force the user to use our high standard for the data. Make your function, by default, report an error if any
-#file has less than 50 observations, but allow the user to override this behavior and only receive a warning if 50
-#observations are not present in a file.
-
-#For an extra credit point, add arguments and associated code to your function to situations where a file
-#doesn’t have the correct number of columns or the provided data includes NA’s.
-
-#Need to figure out how to override a default for a warning not an error. By having two defaults, changing them gives the output of 50+, one 
-#gives an error for being under 50 with req="Yes", and the last gives a Warning for req="No".By changing x, the minimum number of observations
-#is changed. 
+#Usage: VarCof(dir = "full directory map", column = "number of column of interest", 
+#x = minimum number of observations, req = is override on or off (off by default))
 
 VarCof<-function(dir, column, x=50, req="Yes"){
+  #Set the working directory to the directory entered 
+  setwd(dir)
   #List files in a variable
   file.list<-list.files(dir)
   #Empty vector to add results to
   vector<-c()
   #For each of the files, pull out the column of interest
-  for (i in 1:length(file.list)){
+  for (files in file.list){
     #Read the file as a table so the column can be pulled out
-    file<-read.table(file.list[i], header = TRUE, stringsAsFactors = FALSE)
+    file<-read.table(files, header = TRUE, sep=",", stringsAsFactors = FALSE)
     #Pull out the column of interest
     interestcol<-file[, column]
     #Find the number of observations
-    if(length(interestcol)>x){ #More than X, no error message
+    if(length(interestcol)>=x){ #More than or equal to X, no error message
       #Calculate, mean, standard deviation, and variation
       M<-mean(interestcol)
       SD<-sd(interestcol, na.rm = FALSE)
       Var<-SD/M
       vector<-c(vector, Var)
-      }else if (length(interestcol)<x && req = "Yes"){
+      }else if (length(interestcol)<x && req == "Yes"){ #Less than X, no override, gives warning 
       print("Error: There are not enough observations to calculate a reliable coefficient of variation")
-        vector<-c(vector, NA)
-      }else if (length(interestcol)<x && req = "No"){
+        #vector<-c(vector, NA)
+      }else if (length(interestcol)<x && req == "No"){ #Less than X, override. gives warning but solves
         print("Warning: There are not enough observations to calculate a reliable coefficient of variation")
         M<-Mean(interestcol)
         SD<-sd(interestcol)
         Var<-SD/M
         vector<-c(vector, Var)
+      }else{
+        print("Check arguement inputs")
       }
      }
     return(vector)
