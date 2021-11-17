14cd978a07bcf0324485fcc2529399f40d590b2b
Johanna1403 <johannaolesk99@gmail.com>
2021-11-16 21:53:14 -0500

    Initial commit. Needs debugging

---
+++ Exercise9.R
@@ -0,0 +1,48 @@
+# Exercise 9
+# here it is assumed that all the files are CSV files (although the user can specify the delimiter type)
+# works only if the directory contains files that have the same separator (here the user can choose between comma or tab separated files)
+
+
+VarCoefficient <- function(dir, column, observations = 0) {
+  setwd(dir)
+  list_files = list.files(path = dir)
+  variations = NULL
+  for (f in 1:length(list_files)) {
+    data <- read.csv(file = list_files[f], header = T, sep = ",", stringsAsFactors = FALSE)
+    obs = nrow(data)
+    if (observations == 0){
+      if (obs < 50) {
+        print("Error! Too few observations!")
+        next
+      }
+      else {
+        sd_col = sd(data[, column])
+        mean_col = mean(data[, column])
+        variationCoeff = sd_col / mean_col
+        variations = c(variations, variationCoeff)
+      }
+    }
+    else if (observations == 1) {
+      if (obs < 50) {
+        print("Warning! Less than 50 observations.")
+        sd_col = sd(data[, column])
+        mean_col = mean(data[, column])
+        variationCoeff = sd_col / mean_col
+        variations = c(variations, variationCoeff)
+      }
+      else {
+        sd_col = sd(data[, column])
+        mean_col = mean(data[, column])
+        variationCoeff = sd_col / mean_col
+        variations = c(variations, variationCoeff)
+      }
+    }
+  }
+  return(variations)
+}
+
+VarCoefficient("/Users/johi/Desktop/Courses/Biocomputing/r-novice-inflammation/infl_data", 2, observations = 1)
+
+
+
+
