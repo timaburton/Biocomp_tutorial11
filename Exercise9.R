e79db0fc6ed0fadfeb2301e18e284521efbca637
Johanna1403 <johannaolesk99@gmail.com>
2021-11-16 23:27:22 -0500

    Debugged and implemented code.

--- Exercise9.R
+++ Exercise9.R
@@ -1,14 +1,15 @@
 # Exercise 9
 # here it is assumed that all the files are CSV files (although the user can specify the delimiter type)
-# works only if the directory contains files that have the same separator (here the user can choose between comma or tab separated files)
+# works only if the directory contains files that have the same separator
 
 
-VarCoefficient <- function(dir, column, observations = 0) {
+
+VarCoefficient <- function(dir, column, observations = 0, delimiter = ",") {
   setwd(dir)
   list_files = list.files(path = dir)
   variations = NULL
   for (f in 1:length(list_files)) {
-    data <- read.csv(file = list_files[f], header = T, sep = ",", stringsAsFactors = FALSE)
+    data <- read.csv(file = list_files[f], header = T, sep = delimiter, stringsAsFactors = FALSE)
     obs = nrow(data)
     if (observations == 0){
       if (obs < 50) {
@@ -16,33 +17,22 @@ VarCoefficient <- function(dir, column, observations = 0) {
         next
       }
       else {
-        sd_col = sd(data[, column])
-        mean_col = mean(data[, column])
-        variationCoeff = sd_col / mean_col
-        variations = c(variations, variationCoeff)
       }
     }
     else if (observations == 1) {
       if (obs < 50) {
         print("Warning! Less than 50 observations.")
-        sd_col = sd(data[, column])
-        mean_col = mean(data[, column])
-        variationCoeff = sd_col / mean_col
-        variations = c(variations, variationCoeff)
       }
       else {
-        sd_col = sd(data[, column])
-        mean_col = mean(data[, column])
-        variationCoeff = sd_col / mean_col
-        variations = c(variations, variationCoeff)
       }
     }
+    sd_col = sd(data[, column])
+    mean_col = mean(data[, column])
+    variationCoeff = sd_col / mean_col
+    variations = c(variations, variationCoeff)
   }
   return(variations)
 }
 
-VarCoefficient("/Users/johi/Desktop/Courses/Biocomputing/r-novice-inflammation/infl_data", 2, observations = 1)
-
-
-
+VarCoefficient("/Users/johi/Desktop/Courses/Biocomputing/r-novice-inflammation/infl_data", 5, observations = 0)
 
