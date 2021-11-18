1edd802e01f8e2b5e5a7520f56c02c066bb51266
Megan Schmidt <mschmi26@nd.edu>
2021-11-18 12:13:30 -0500

    Add 50 if-else

--- Tutorial9.R
+++ Tutorial9.R
@@ -2,38 +2,82 @@ setwd("C:/Users/megan/Desktop/r-novice-inflammation/r-novice-inflammation-data/d
 dir <- list.files("../data", full.names = TRUE)
 dir
 fileCOV <- c()
-
+usable <- c()
 coeffientOfVariation <- function(dir){
   csv <- dir[grep(".csv",dir)]
   table <- dir[grep(".txt",dir)]
   usable <- c(csv,table)
   for (i in 1:length(usable)){
-    if (usable[grep(".csv", usable)] == TRUE){
     fil <- read.csv(file = usable[i], header = TRUE, stringsAsFactors = FALSE)
-    }
-    else {
-      fil <- read.table(file = usable[i], sep = ,header = TRUE, stringsAsFactors = FALSE)
-    }
     y <- sapply(fil,class)
     z <- colnames(fil)
     print(paste("Which column of the file would you like to calculate the coefficient of variation for? The columns are" , z , "/" ,y))
     print("Please only select a column of integers or numerics.")
     x <- as.name(readline())
+    if (length(fil[[x]]) < 50){
+      print("An accurate coefficient of variation should have at least 50 entries, and this column has less than 50.")
+      question <- (readline("Would you like to continue? Yes or No"))
+      if (question == "YES"){
+        mean <- mean(fil[[x]])
+        std <- sd(fil[[x]])
+        cov <- std/mean
+        fileCOV <- c(fileCOV, cov)
+        i = i + 1
+      }else {
+        i = i +1
+      }
+    }
+    else {
     mean <- mean(fil[[x]])
     std <- sd(fil[[x]])
     cov <- std/mean
     fileCOV <- c(fileCOV, cov)
     i = i + 1
+    }
   }
   
 }
-file <- read.csv(file="wages.csv", header = TRUE, stringsAsFactors = FALSE)
+coeffientOfVariation(dir)
+
+
+
+file <- read.csv(file="car-speeds.csv", header = TRUE, stringsAsFactors = FALSE)
 y <- sapply(file,class)
 z <- colnames(file)
 print(paste("Which column of wages.csv would you like to calculate the coefficient of variation for? The columns are" , z , "/" ,y))
 print("Please only select a column of integers or numerics.")
 x <- as.name(readline())
+if (length(file[[x]]) < 50){
+  print("An accurate coefficient of variation should have at least 50 entries, and this column has less than 50.")
+  question <- (readline("Would you like to continue? Yes or No"))
+  if (question == "YES"){
+    mean <- mean(file[[x]])
+    std <- sd(file[[x]])
+    cov <- std/mean
+    fileCOV <- c(fileCOV, cov)
+  }else{
+    print("Okay")
+  }
+}else {
+  mean <- mean(file[[x]])
+  std <- sd(file[[x]])
+  cov <- std/mean
+  fileCOV <- c(fileCOV, cov)
+}
+
+
+length(file$wage)
+
 mean <- mean(file[[x]])
 std <- sd(file[[x]])
 cov <- std/mean
 fileCOV <- c(fileCOV, cov)
+
+if (question == "YES"){
+  mean <- mean(file[[x]])
+  std <- sd(file[[x]])
+  cov <- std/mean
+  fileCOV <- c(fileCOV, cov)
+}else{
+  print("Okay")
+}
