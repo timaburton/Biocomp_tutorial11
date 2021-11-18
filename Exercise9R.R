c1d5ab87af889f125466b2fad8cd5a92d39a30bc
Marlee Shaffer <mshaffe2@nd.edu>
2021-11-18 11:27:53 -0500

    Made changes to get correct variance. Working on changing defaults and still getting outputs

--- Exercise9R.R
+++ Exercise9R.R
@@ -6,43 +6,52 @@
 #to get an output. The column of interest must be numerical.
 
 #Usage: VarCof(dir = "full directory map", column = "number of column of interest", 
-#x = minimum number of observations, req = is override on or off (off by default))
+#x = minimum number of observations (by default, it is 50))
 
-VarCof<-function(dir, column, x=50, req="Yes"){
-  #Set the working directory to the directory entered 
+VarCof<-function(dir, col, x="50", override="No"){
+  #Set the working directory to the directory entered
   setwd(dir)
-  #List files in a variable
+  #List all files in the directory and save them as a variable
   File<-list.files(dir)
-  #Empty vector to add results to
-  Variation<-c()
-  #For each of the files, pull out the column of interest
+  #Create an empty vector for variance
+  Variance<-c()
+  #Create an empty vector for error
+  Error<-c()
+  #For each file..
   for (files in File){
-    #Read the file as a table so the column can be pulled out
-    file_i<-read.table(files, header = TRUE, sep=",", stringsAsFactors = FALSE)
-    #Pull out the column of interest
-    interestcol<-file_i[, column]
-    #Find the number of observations
-    if(length(interestcol)>=x){ #More than or equal to X, no error message
-      #Calculate, mean, standard deviation, and variation
-      M<-mean(interestcol)
-      SD<-sd(interestcol, na.rm = FALSE)
+    #Read the file as a table so the columns can be pulled out 
+    file_i<-read.table(files, header = TRUE, sep=",", stringsAsFactors = FALSE, fill = TRUE)
+    #Pull out column of interest
+    interestcol<-file_i[, col]
+    interestcol.na<-na.omit(interestcol)
+    #Find the number of observations and do math depending on value of x and override
+    if (length(interestcol.na)>=x){ #Greater than or equal to 50, or changed value
+      #Calculate the mean, standard deviation, and variation
+      M<-mean(interestcol.na)
+      SD<-sd(interestcol.na, na.rm = FALSE)
       Var<-SD/M
-      Variation<-c(Variation, Var)
-      }else if (length(interestcol)<x && req == "Yes"){ #Less than X, no override, gives warning 
-      print("Error: There are not enough observations to calculate a reliable coefficient of variation")
-        #vector<-c(vector, NA)
-      }else if (length(interestcol)<x && req == "No"){ #Less than X, override. gives warning but solves
-        print("Warning: There are not enough observations to calculate a reliable coefficient of variation")
-        M<-Mean(interestcol)
-        SD<-sd(interestcol)
-        Var<-SD/M
-        Variation<-c(Variation, Var)
-      }else{
-        print("Check arguement inputs")
-      }
-     }
-    return(data.frame(File, Variation))
+      Variance<-c(Variance, Var)
+      Er<-0
+      Error<-c(Error, Er)
+    }else if (length(interestcol.na)<x && override == "Yes"){
+      #Calculate the mean, standard deviation, and variation, but also give a warning
+      M1<-mean(interestcol.na)
+      SD1<-sd(interestcol.na, na.rm = FALSE)
+      Var1<-M1/SD1
+      Variance<-c(Variance, Var1)
+      Er1<-1
+      Error<-c(Error, Er1)
+    }else{
+      Var2<-0
+      Variance<-c(Variance, Var2)
+      Er2<-2
+      Error<-c(Error, Er2)
+    }
   }
-
-
+  Error0<-print("Error 0: No Error, congrats.")
+  Error1<-print("Error 1:  Warning: There are not enough observations for a reliable calculation, but this error was overlooked by your inputs")
+  Error2<-print("Error 2: Error: There are not enough observations, please add more.")
+  return(data.frame(File, Variance, Error)) 
+  return(Error0,Error1,Error2)
+}
 
