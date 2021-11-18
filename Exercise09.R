f655427130b64dd910e088d40b4f75f149a5b6e2
Helen Laboe <hlaboe@nd.edu>
2021-11-17 22:57:24 -0500

    Adding commentary

--- Exercise09.R
+++ Exercise09.R
@@ -11,10 +11,13 @@ CoefficientofVariation <- function(dir,col,nrow=50){
   # Sets up a loop for the files in the directory to calculate the coefficient of variation
   for (i in inputfiles){
     i <- read.csv(i,header=TRUE,sep=",",stringsAsFactors=FALSE, fill=TRUE)
+    # Checking if the file has the correct number of columns
     if(ncol(i)< col){
       print("Invalid column number.")
     }else{
+      # Checking if the column has 50 observations
       if(nrow(i) <50){
+        # Offers the override option if the column does not have 50 observations
         not50 <- print("Recommended that the file has at least 50 observations to calculate a coefficient of variation. Would you like to proceed? Type 'Y' for Yes and 'N' for No: ")
         if(not50=="Y"){
           mean=mean(i[,col])
@@ -22,6 +25,7 @@ CoefficientofVariation <- function(dir,col,nrow=50){
           coefficientofvariation=standarddeviation/mean
           coefficientofvariationVector=c(coefficientofvariationVector,coefficientofvariation)
         }else{
+          # Output if the user chooses not to override
           print("Function stopped.")
         }
       }else{
