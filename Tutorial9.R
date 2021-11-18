dca8ea9a002df719e1752e2b34c0d55ecd4be708
Megan Schmidt <mschmi26@nd.edu>
2021-11-17 23:47:36 -0500

    progress

--- Tutorial9.R
+++ Tutorial9.R
@@ -1,18 +1,39 @@
 setwd("C:/Users/megan/Desktop/r-novice-inflammation/r-novice-inflammation-data/data")
 dir <- list.files("../data", full.names = TRUE)
-
+dir
 fileCOV <- c()
+
 coeffientOfVariation <- function(dir){
-  
+  csv <- dir[grep(".csv",dir)]
+  table <- dir[grep(".txt",dir)]
+  usable <- c(csv,table)
+  for (i in 1:length(usable)){
+    if (usable[grep(".csv", usable)] == TRUE){
+    fil <- read.csv(file = usable[i], header = TRUE, stringsAsFactors = FALSE)
+    }
+    else {
+      fil <- read.table(file = usable[i], sep = ,header = TRUE, stringsAsFactors = FALSE)
+    }
+    y <- sapply(fil,class)
+    z <- colnames(fil)
+    print(paste("Which column of the file would you like to calculate the coefficient of variation for? The columns are" , z , "/" ,y))
+    print("Please only select a column of integers or numerics.")
+    x <- as.name(readline())
+    mean <- mean(fil[[x]])
+    std <- sd(fil[[x]])
+    cov <- std/mean
+    fileCOV <- c(fileCOV, cov)
+    i = i + 1
+  }
   
 }
-wage <- read.csv(file="wages.csv", header = TRUE, stringsAsFactors = FALSE)
-y <- sapply(wage,class)
-z <- colnames(wage)
+file <- read.csv(file="wages.csv", header = TRUE, stringsAsFactors = FALSE)
+y <- sapply(file,class)
+z <- colnames(file)
 print(paste("Which column of wages.csv would you like to calculate the coefficient of variation for? The columns are" , z , "/" ,y))
 print("Please only select a column of integers or numerics.")
 x <- as.name(readline())
-mean <- mean(wage[[x]])
-std <- sd(wage[[x]])
+mean <- mean(file[[x]])
+std <- sd(file[[x]])
 cov <- std/mean
 fileCOV <- c(fileCOV, cov)
