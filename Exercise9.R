911bfee7ad87c0afb818b27985f89e499ae256b9
Casey Novak <cnovak@nd.edu>
2021-11-19 17:17:45 -0500

    Final answers for exercise 9

--- Exercise9.R
+++ Exercise9.R
@@ -1,43 +1,38 @@
 # Exercise 09 
 
-setwd("~/Desktop/Fall-2021/Biocomputing")
+setwd("~/Desktop/Fall-2021/Biocomputing/Biocomp_tutorial11")
 # Name of function and arguments
 
-dir="~/Desktop/Fall-2021/Biocomputing/Biocomp_tutorial11/Example/"
-column=1
-
-# Takes directory as argument, specifies which column from each file to be used in calculations 
+# Takes a directory path and column number, default arguments are set for rows and override 
 exercise9 <- function(dir, column, rows=50, override=FALSE){
 # Reads data from each file in the specified directory
   file_list <- list.files(path=dir) # This function assumes that all of the files in the directory are .csv files 
-  results<-NA
-  for (i in 1:length(file_list)){
+  results<-NA # Creates results vector 
+  for(i in 1:length(file_list)){ 
     data <- read.table(file_list[i])
-    if(nrow(data)<rows){
-      print("Error because number of rows in file does not meet the minimum requirement.")
-      if (override==TRUE){
-        # Save data and calculate coefficient of variation 
+    if(nrow(data)<rows){ # Tests whether the number of rows in each file meets the specified row requirement
+      print(paste0("Error because number of rows in ", file_list[i], " does not meet the minimum requirement."))
+      if (override==TRUE){ # If override is true, then the coefficient of standard deviation will still be calculated 
+        print(paste0("Coefficient of variation for ", file_list[i], " will be added to results vector even though file did not meet row requirement."))
         stdev <- sd(data[,column])
-        mean <- mean(data$column[i]==column)
-        print("Coefficient of variation will be added to results even though file did not meet row requirement.")
+        mean <- mean(data[,column])
         results <- c(results, (stdev/mean))
-      }else{
+      }else{ # If override is false and rows do not meet the requirement, NA will be added to the results vector 
         results <- c(results, NA) 
         }
     }else{
-      # Calculate the standard deviation 
+      # If rows are greater than 50, add the coefficient of standard deviation to the results vector for each file 
       stdev <- sd(data[,column])
-      mean <- mean(data$column[i]==column)
+      mean <- mean(data[,column])
       results <- c(results, (stdev/mean))
     } 
   }
   return(results[-1])
 }
 
-
+# Call function 
+## Give path to directory for dir argument
+## Give number of column for column argument 
+## Optional to change default number of rows and override
+exercise9(dir,column)
   
-
-
-
-
-# Create directory with two csv files and have one column for each 
