738ad5a6bc63025907f44f941662192d9d844bd7
Erin Maron <erinmaron@erins-mbp.dhcp.nd.edu>
2021-11-18 20:39:39 -0500

    Final Version Maron

--- MaronTutorial11.R
+++ MaronTutorial11.R
@@ -1,18 +1,19 @@
 #Tutorial Assignment 12
 setwd("/Users/erinmaron/Desktop/r-novice-inflammation/BioComp/ClassWork/Biocomp_tutorial11")
 
-#Actual Function 
-dir<-function(x,y){
-  input<-list.files(x, pattern="*.csv",full.names = TRUE)
-  data<-lapply(input,read.csv)
-  stdev<-sd(data$y)
-  mean<-mean(data$y)
-  print((stdev/mean)*100)
-  if (nrow(file)<50){
-    print("warning: low level of data")
-  }else if (nrow(file)>50){
-    print("warning: entry does not have 50 entries")
-  }
+#Calling Directory
+dir<-function(directory,column){
+  files<-list.files(pattern="*.csv") #pulls list of files from directory
+  for(i in 1:length(files)){
+    data<-read.table(files[i]) #read files within directory 
+    stdev<-sd(data$column==column, na.rm=FALSE) #find and set the standard deviation of data
+    mean<-mean(data$column==column) #repeat with the mean 
+    }
+  if(nrow(files>=50)){
+      return((stdev/mean)*100) #if the data has 50 or more points, print an error 
+    }else{
+      print("Warning:Less than 50 data point", cdev) #if there is insufficient data, print an error but continue to prvide output
+    }
 }
-  
 
+#Keep getting error with calling a column name in arguement, but cannot seem to find what to fix after looking at notes and googling, so this is as far as I could get
